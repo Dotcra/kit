6f803f82d68153d38373c50d58f9cdce4257aaa6
Dot <dotcra@gmail.com>
2018-01-11 11:16:30 +0800

    nth

---
+++ src/Cat.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * vim:ft=php
+ * @author Dotcra <dotcra@gmail.com>
+ * @version
+ * @todo
+ */
+
+class Cat{
+	private $a;
+	private $b;
+	private $c;
+	function __construct(){
+	}
+
+	function __destruct(){
+	}
+
+	function say(){
+		echo "haha, you suck";
+	}
+}
+
+#new pus();
+#pus::f();
+
